8da15f4cd24707e87f3d8b5f7100de822eb93dc8
James Oakes <cheerlesssnake@gmail.com>
2025-09-23 22:56:52 -0500

    added process detect, persistence, network, and usb connections

--- triage.bat
+++ triage.bat
@@ -34,3 +34,61 @@ echo. >> triage.txt
 
 ::Running Processess and Services::
 echo --- RUNNING PROCESSES --- >> triage.txt
+tasklist >> triage.txt
+echo. >> triage.txt
+echo --- RUNNING SERVICES --- >> triage.txt
+echo Saving running services to services.txt... >> triage.txt
+sc query > services.txt
+echo. >> triage.txt
+
+::Networking::
+echo --- NETWORK INFORMATION --- >> triage.txt
+echo IP Configuration: >> triage.txt
+ipconfig /all >> triage.txt
+echo. >> triage.txt
+echo Active Network Connections: >> triage.txt
+netstat -ano >> triage.txt
+echo. >> triage.txt
+echo ARP Cache: >> triage.txt
+arp -a >> triage.txt
+echo. >> triage.txt
+echo Routing Table: >> triage.txt
+route print >> triage.txt
+echo. >> triage.txt
+echo DNS Cache: >> triage.txt
+ipconfig /displaydns >> triage.txt
+echo. >> triage.txt
+echo Hosts File Content: >> triage.txt
+type C:\Windows\System32\drivers\etc\hosts >> triage.txt
+echo. >> triage.txt
+echo Windows Firewall Status: >> triage.txt
+netsh advfirewall show allprofiles >> triage.txt
+echo. >> triage.txt
+
+::Persistence and Startup Items::
+echo --- PERSISTENCE AND STARTUP ITEMS --- >> triage.txt
+echo Saving scheduled tasks to scheduledtasks.txt... >> triage.txt
+schtasks /query /fo LIST /v > scheduledtasks.txt
+echo. >> triage.txt
+echo Saving Registry autostart information from HKLM\Software\Microsoft\Windows\CurrentVersion\Run and HKCU\Software\Microsoft\Windows\CurrentVersion\Run in separate files... >> triage.txt
+reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" > HKLM_Run.txt
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" > HKCU_Run.txt
+echo. >> triage.txt
+echo Listing contents of common startup folders... >> triage.txt
+echo Listing "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"... >> triage.txt
+dir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" >> triage.txt
+echo. >> triage.txt
+echo Listing "%APPDATA%\Microsoft\Windows\Start Menu\Programs\StartUp"... >> triage.txt
+dir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\StartUp" >> triage.txt
+echo. >> triage.txt
+
+::Devices and Storage::
+::teammates write your code here
+
+::Logs & event traces::
+::teammates write your code here. I also suggest saving logs to a separate file instead of the triage.txt file because log files can be very long.
+
+::Current USB Device connections::
+echo --- USB DEVICE INFORMATION --- >> triage.txt
+wmic path Win32_PnPEntity where "DeviceID like 'USB%%'" get Name,DeviceID > connectedusbdevices.txt
+echo. >> triage.txt
